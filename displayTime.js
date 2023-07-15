7cbfd60c6af3179160a4a38d97b5b69644b2eca8
GoddessVenus <41741293+GoddessVenus@users.noreply.github.com>
2023-07-15 21:20:09 +0800

    Add files via upload

--- displayTime.js
+++ displayTime.js
@@ -1,41 +1,22 @@
 function displayTime()
-
 {
-
 var today=new Date();
-
 var h=today.getHours();
-
 var m=today.getMinutes();
-
 var s=today.getSeconds();
-
 // add a zero in front of numbers<10
-
 m=checkTime(m);
-
 s=checkTime(s);
-
-document.getElementById('displayTime').innerHTML=h+":"+m+":"+s;
-
+const ele = document.getElementById('displayTime');
+if(ele)ele.innerHTML=h+":"+m+":"+s;
 t=setTimeout('displayTime()',500);
-
 }
 
-function checkTime(i)
-
-{
-
-if (i<10)
-
-{
-
+function checkTime(i){
+if (i<10){
 i="0" + i;
-
 }
-
 return i;
-
 }
 
 displayTime();
